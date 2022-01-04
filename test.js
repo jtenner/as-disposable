b1d5e57d079ca1c1dcf1b6ffe1f28141904eb861
Joshua Tenner <tenner.joshua@gmail.com>
2022-01-04 13:09:59 -0500

    use rtrace in test to validate

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 const loader = require("assemblyscript/lib/loader");
+const rtrace = require("assemblyscript/lib/rtrace");
+
 const fs = require("fs");
 
 const buffer = fs.readFileSync("./build/test.wasm");
 
-const api = loader.instantiateSync(buffer, {});
+
+const memory = new WebAssembly.Memory({
+    initial: 10,
+});
+let rt = new rtrace.Rtrace({
+    getMemory() {
+        return memory;
+    }
+});
+
+const api = loader.instantiateSync(buffer, rt.install({ memory }));
 
 let memoryPtr = api.exports.setup();
 api.exports.run();
